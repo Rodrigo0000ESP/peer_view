f0f44088e5c3023e899e7b49319b4c0963ccfcae
Rodrigo <cordejavihd@gmail.com>
2025-12-07 12:24:27 +0000

    Remove node_modules from tracking

---
+++ .next/dev/build/chunks/[turbopack-node]_transforms_postcss_ts_f1afe608._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Desktop/peer-view-landing-page/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "chunks/518dd_b043db6c._.js",
+  "chunks/[root-of-the-server]__efc4a26d._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Desktop/peer-view-landing-page/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
---
+++ .next/dev/static/chunks/Desktop_peer-view-landing-page_app_page_tsx_f8d28306._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/518dd_0bd616b7._.js",
+  "static/chunks/Desktop_peer-view-landing-page_bf4d00e0._.js"
+],
+    source: "dynamic"
+});
